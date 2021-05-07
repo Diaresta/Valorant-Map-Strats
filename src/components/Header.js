06fb98efe4e4dbb7ee3b1792de28f77fcccb1318
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-05-06 21:09:06 -0400

    Header test

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,18 @@ const Header = ({ title }) => {
         <p>Valorant Strat Planner</p>
       </div>
       <div>
-        <a href='https://github.com/Diaresta/Valorant-Map-Strats'>Github</a>
-        <a href='https://github.com/Diaresta/Valorant-Map-Strats'>Home</a>
+        <a
+          href='https://github.com/Diaresta/Valorant-Map-Strats'
+          target='_blank'
+        >
+          Github
+        </a>
+        <a
+          href='https://github.com/Diaresta/Valorant-Map-Strats'
+          target='_blank'
+        >
+          Home
+        </a>
       </div>
       {/* <img src={process.env.PUBLIC_URL + '/shine-logo.png'} alt='reee' /> */}
     </header>
