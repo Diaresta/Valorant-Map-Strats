296ddf1eeaa60d9cfdd7402ab706d1c9d090ac91
Diaresta <TheAndrewRobles@yahoo.com>
2021-05-08 17:44:53 -0400

    Image file(s) adjustment

--- src/components/agent-info.js
+++ src/components/agent-info.js
@@ -6,7 +6,6 @@ const agentInfo = [
     abil_1: '/images/Agent-Images/Astra/Abil-1.png',
     abil_2: '/images/Agent-Images/Astra/Abil-2.png',
     abil_3: '/images/Agent-Images/Astra/Abil-3.png',
-    abil_4: '/images/Agent-Images/Astra/Abil-4.png',
   },
 ];
 
