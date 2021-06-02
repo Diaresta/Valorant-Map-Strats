5cbfd66859bb4d85dff3bb500f06e7932bee979e
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-06-02 15:57:05 -0400

    Prepping (potential) eSports page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-// import './static/css/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import SiteCompile from './components/SiteCompile';
+import Esports from './components/Esports';
 import ErrorPage from './components/ErrorPage';
 
 function App() {
+  // const [apiData, setApiData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const url = await axios(`https://api.randomuser.me`);
+
+  //     console.log(url.data);
+  //     setApiData(url.data);
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // -----------------------------------------
+  // const [items, setItems] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const fetchItems = async () => {
+  //     const result = await axios(`https://pokeapi.co/api/v2/pokemon`);
+
+  //     console.log(result.data.results);
+  //     setItems(result.data);
+  //     setIsLoading(false);
+  //   };
+
+  //   fetchItems();
+  // }, []);
+
   return (
     <div className='App'>
       <Router>
@@ -12,6 +44,11 @@ function App() {
         <Switch>
           <Route path={['', '/', '/home']} exact>
             <SiteCompile />
+          </Route>
+          <Route path={'/esports'} exact>
+            {/* <Esports loading={loading} apiData={apiData} /> */}
+            {/* <Esports isLoading={isLoading} items={items} /> */}
+            <Esports />
           </Route>
           <Route path='*' component={ErrorPage} exact />
         </Switch>
---
+++ src/components/Esports.js
@@ -0,0 +1,19 @@
+const Esports = ({ isLoading, items }) => {
+  //   return isLoading ? (
+  //     <h1 style={{ color: 'white' }}>Loading...</h1>
+  //   ) : (
+  //     <section>
+  //       {items.map((item) => (
+  //         <h1>{item}</h1>
+  //       ))}
+  //     </section>
+  //   );
+
+  return (
+    <div>
+      <h1 style={{ color: 'white' }}>Loading...</h1>
+    </div>
+  );
+};
+
+export default Esports;
